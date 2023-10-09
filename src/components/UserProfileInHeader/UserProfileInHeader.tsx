d4965c004fcee04102f61d0414d68faf711f6df7
FranciszekKaniewski <fkaniewski.web.com>
2023-10-09 09:21:40 +0200

    Open list of subpages in UserProfileInHeader added and styled, change Header.css and UserProfileInHeader.css styles

--- src/components/UserProfileInHeader/UserProfileInHeader.tsx
+++ src/components/UserProfileInHeader/UserProfileInHeader.tsx
@@ -5,11 +5,27 @@ import './UserProfileInHeader.css'
 export const UserProfileInHeader = () =>{
 
     const [user,setUser]= useState(0);
+    const [optionsOpened,setOptionsOpened]= useState(false);
+
+    const showOptions = () =>{
+        setOptionsOpened(prevState => !prevState)
+        console.log(optionsOpened)
+    }
 
     return(
         <div className="user">
             <img src="" alt="user-profile-img"/>
-            {!user ? <h3>Franciszek Kaniewski</h3>:<h3>Login</h3>}
+            {!user ?
+                <div className="user-name">
+                    <h3 onClick={showOptions}>Franciszek Kaniewski</h3>
+                    {optionsOpened?<ul>
+                        <li>⚙ Settings</li>
+                        <li>🐾 Profile</li>
+                        <li>🚪 Logout</li>
+                    </ul>:null}
+                </div>
+                :
+                <h3>Login</h3>}
         </div>
     )
 }
