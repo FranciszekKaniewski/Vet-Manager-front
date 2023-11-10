f84ad245e309f5c3ec7fa43f6dd692c2da36c6a9
FranciszekKaniewski <fkaniewski.web.com>
2023-11-10 20:59:43 +0100

    Fetch default method change

--- src/utils/Fetch.tsx
+++ src/utils/Fetch.tsx
@@ -1,9 +1,9 @@
-export const Fetch = async (url:string,method:string="GET",body?:any) =>{
+export const Fetch = async (url:string,method?:string,body?:any) =>{
 
     try {
 
         return await fetch(`https://megak-vet-manager-bb4d119f3444.herokuapp.com/${url}`, {
-            method: method,
+            method: method ? method : "GET",
             mode: "cors",
             credentials: 'include',
             headers: {
