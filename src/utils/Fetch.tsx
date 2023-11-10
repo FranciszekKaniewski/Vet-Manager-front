54c1808490a9a9e9fc4fde34ff06f66fa09ac7f1
FranciszekKaniewski <fkaniewski.web.com>
2023-11-10 18:34:43 +0100

    Change fetch address

--- src/utils/Fetch.tsx
+++ src/utils/Fetch.tsx
@@ -2,7 +2,7 @@ export const Fetch = async (url:string,method:string="GET",body?:any) =>{
 
     try {
 
-        return await fetch(`http://localhost:3001/${url}`, {
+        return await fetch(`https://megak-vet-manager-bb4d119f3444.herokuapp.com/${url}`, {
             method: method,
             mode: "cors",
             credentials: 'include',
